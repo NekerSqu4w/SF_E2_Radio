2a9e1716d8af383a0bcd8e6e923c82e1e5c4034d
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2023-02-16 10:47:49 +0100

    aaaa

---
+++ song_loader_example.lua
@@ -0,0 +1,36 @@
+--@name Song loader example
+--@author AstalNeker
+
+--Include loader as song_loader.lua
+--@include https://github.com/NekerSqu4w/SF_E2_Radio/blob/main/song_loader.lua?raw=true as song_loader.lua
+
+if CLIENT then
+    --Import the loader
+    local song_loader = require("song_loader.lua")
+    
+    --Some value to choose your song
+    local list = "mp3" --Two list exist
+    local load_id = 15
+    
+    --Used to get data information
+    song_loader.get_data(function(data)
+        print("Playlist data version: " .. data.version)
+    end)
+
+    --Used to load playlist data
+    song_loader.get_list(function(playlist)
+        print("Current list as " .. #playlist[list] .. " file")
+        print("Current song: " .. playlist[list][load_id].title)
+        print("Playlist name: " .. playlist.list_name[list])
+
+        bass.loadURL(playlist[list][load_id].link, "noblock", function(snd,error)
+            if not error then
+                snd:setVolume(1)
+                if not snd:isOnline() then
+                    snd:setLooping(true)
+                end
+            end
+        end)
+
+    end)
+end
