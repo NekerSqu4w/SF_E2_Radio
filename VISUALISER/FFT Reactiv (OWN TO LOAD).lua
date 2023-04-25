c3b4b8cfc15e8c06f8fd34114ebb218832da2dec
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2023-04-25 14:42:48 +0200

    how to load fft reactiv

---
+++ VISUALISER/FFT Reactiv (OWN TO LOAD).lua
@@ -0,0 +1,21 @@
+--@name FFT Reactiv (LOADER)
+--@author AstalNeker
+--@client
+
+//own library
+--@include https://raw.githubusercontent.com/NekerSqu4w/my-starfall-library/main/retro_screen.txt as retro_screen.txt
+--@include https://raw.githubusercontent.com/NekerSqu4w/my-starfall-library/main/multiple_screen_lib.txt as multiple_screen_lib.txt
+--@include https://raw.githubusercontent.com/NekerSqu4w/my-starfall-library/main/permission_handler.txt as permission_handler.txt
+--@include https://raw.githubusercontent.com/NekerSqu4w/my-starfall-library/main/uuid.txt as uuid.txt
+--@include https://raw.githubusercontent.com/NekerSqu4w/SF_E2_Radio/main/loader.lua as loader.lua
+
+//gui library
+--@include https://raw.githubusercontent.com/itisluiz/SFUi/main/sfui/sfui.lua as sfui.lua
+--@include https://raw.githubusercontent.com/itisluiz/SFUi/main/sfui/components/component.lua as components/component.lua
+--@include https://raw.githubusercontent.com/itisluiz/SFUi/main/sfui/components/window.lua as components/window.lua
+--@include https://raw.githubusercontent.com/itisluiz/SFUi/main/sfui/components/button.lua as components/button.lua
+--@include https://raw.githubusercontent.com/itisluiz/SFUi/main/sfui/components/list.lua as components/list.lua
+
+
+--@include https://raw.githubusercontent.com/NekerSqu4w/SF_E2_Radio/main/VISUALISER/FFT%20Reactiv.lua as fft.lua
+require("fft.lua")
