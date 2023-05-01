3bba0dfe1a7d8bdb783697295ea6e31da74c6df3
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2023-05-01 17:44:23 +0200

    test

--- VISUALISER/custom/reactiv.lua
+++ VISUALISER/custom/reactiv.lua
@@ -1,10 +1,10 @@
 local visualizer = {}
-visualizer.data_onload = "test"
+visualizer.smooth_fft = {}
 
 function visualizer.render(loader,me)
     render.drawSimpleText(64,64,"Hello it's a test")
     render.drawSimpleText(64,64+15,"Data from visualizer: " .. table.toString(me))
-    render.drawSimpleText(64,64+30,"Data from loader: " .. table.toString(loader,nil,true))
+    render.drawText(64,64+30,"Data from loader: " .. table.toString(loader,nil,true))
 end
 
 return visualizer
