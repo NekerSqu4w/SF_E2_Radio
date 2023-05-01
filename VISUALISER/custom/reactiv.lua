f057f81cddd586ba3522ba86797daa79feffebf7
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2023-05-01 17:42:02 +0200

    ttest

--- VISUALISER/custom/reactiv.lua
+++ VISUALISER/custom/reactiv.lua
@@ -4,6 +4,7 @@ visualizer.data_onload = "test"
 function visualizer.render(loader,me)
     render.drawSimpleText(64,64,"Hello it's a test")
     render.drawSimpleText(64,64+15,"Data from visualizer: " .. table.toString(me))
+    render.drawSimpleText(64,64+30,"Data from loader: " .. table.toString(loader,nil,true))
 end
 
 return visualizer
